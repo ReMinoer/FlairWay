ac138a4d97ea1bec8c3f81fd07445fb3036387fe
Rémi Morillon <rmorillon@freneticexperience.com>
2017-04-21 13:50:38 +0200

    Add test scene

---
+++ Assets/Scripts/FixedCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FixedCamera : MonoBehaviour
+{
+    public Transform Target;
+    private Vector3 _relativePosition;
+
+    private void Start()
+    {
+        _relativePosition = transform.position - Target.position;
+    }
+
+	private void Update()
+    {
+		transform.position = Target.position + _relativePosition;
+	}
+}
---
+++ Assets/Scripts/SwingController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SwingController : MonoBehaviour
+{
+    public Vector3 Direction;
+    public float Power;
+    public float Drag = 20;
+
+    private void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody>().AddForce(Direction * Power);
+	}
+
+    //private void OnCollisionEnter(Collision other)
+    //{
+    //    GetComponent<Rigidbody>().drag = Drag;
+    //    GetComponent<Rigidbody>().angularDrag = Drag;
+    //}
+
+    //private void OnCollisionExit(Collision other)
+    //{
+    //    GetComponent<Rigidbody>().angularDrag = 0;
+    //    GetComponent<Rigidbody>().angularDrag = 0;
+    //}
+
+    //private void OnCollisionStay(Collision other)
+    //{
+    //    GetComponent<Rigidbody>().drag = Drag;
+    //    GetComponent<Rigidbody>().angularDrag = Drag;
+    //}
+}
